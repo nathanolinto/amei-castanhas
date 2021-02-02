4dd130258f29e83e309e91bce18468ce5f24c385
nathanolinto <nathanolinto@gmail.com>
2021-02-02 12:33:05 -0200

    adicionando api

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,25 @@ function Home(){
     return  <div>Home</div>
 }
 
+function Tempo(props){
+    return (
+        <div>
+            <div></div>
+            <div>{props.staticDateString}</div>
+        </div>
+    )
+}
+
+export async function getStaticProps() {
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+
+    return{
+        props: {
+            staticDateString
+        },
+        revalidate: 3
+    }
+}
+
 export default Home
